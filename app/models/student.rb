00fb9ee71cc697fe49e389c5fb1d251bd8dbc56f
Hannah McCullough <hmac091@gmail.com>
2022-01-31 16:30:29 -0700

    commit tree 1ebf94d694731203e574161af7599bece6c6e644

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,8 @@
 class Student < ApplicationRecord
 
+  # class method written using a scope
+  scope :by_name, ->(name) { where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") }
+
   def to_s
     "#{first_name} #{last_name}"
   end
